a6af25ef86048c905252480c09c9156a525e67b3
Nick Holvast <nicholas.holvast@dwp.gsi.gov.uk>
2018-03-09 20:45:30 +0000

    added jasmine and config

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ module.exports = (function () {
   const firstNames = playerSeed.players.map(player => player.name.first)
   const surnames = playerSeed.players.map(player => player.name.last)
 
-  var firstNameChain = new Foswig(2)
+  var firstNameChain = new Foswig(3)
   firstNameChain.addWordsToChain(firstNames)
 
-  var surnameChain = new Foswig(2)
+  var surnameChain = new Foswig(3)
   surnameChain.addWordsToChain(surnames)
 
-  const firstName = firstNameChain.generateWord(4, 20, false)
-  const surname = surnameChain.generateWord(4, 20, false)
+  const firstName = firstNameChain.generateWord(4, 20, true)
+  const surname = surnameChain.generateWord(4, 20, true)
 
   console.log(`firstName: ${firstName}`)
   console.log(`surname: ${surname}`)
